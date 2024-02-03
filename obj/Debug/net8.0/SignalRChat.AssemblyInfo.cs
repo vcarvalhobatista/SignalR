ab35805f7a187f35f0041c44c2584ff5a89a6748
Vinícius Carvalho Batista <vinicius_carva@hotmail.com>
2024-02-03 02:06:25 -0300

    new commit

--- obj/Debug/net8.0/SignalRChat.AssemblyInfo.cs
+++ obj/Debug/net8.0/SignalRChat.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SignalRChat")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0da4a9045500a1b45635b4fd0edc6b72433b172")]
 [assembly: System.Reflection.AssemblyProductAttribute("SignalRChat")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SignalRChat")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
